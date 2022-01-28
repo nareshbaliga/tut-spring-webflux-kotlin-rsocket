98512c45988df1dfc2beb6e65aad1c14f5c39617
Naresh Baliga <nbaliga@clearcover.com>
2022-01-28 13:43:47 -0600

    Delay with RestTemplate.

--- src/main/kotlin/com/example/kotlin/chat/controller/MessageResource.kt
+++ src/main/kotlin/com/example/kotlin/chat/controller/MessageResource.kt
@@ -3,6 +3,7 @@ package com.example.kotlin.chat.controller
 import com.example.kotlin.chat.service.MessageService
 import com.example.kotlin.chat.service.MessageVM
 import org.slf4j.LoggerFactory
+import org.springframework.http.HttpMethod
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
@@ -10,11 +11,13 @@ import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.client.RestTemplate
 
 @RestController
 @RequestMapping("/api/v1/messages")
 class MessageResource(val messageService: MessageService) {
     private val logger = LoggerFactory.getLogger(javaClass)
+    private val restTemplate = RestTemplate()
 
     @GetMapping
     fun latest(@RequestParam(value = "lastMessageId", defaultValue = "") lastMessageId: String): ResponseEntity<List<MessageVM>> {
@@ -42,7 +45,18 @@ class MessageResource(val messageService: MessageService) {
         @RequestParam(value = "delay", defaultValue = "1000") delayMs: Long
     ): Iterable<MessageVM> {
         logger.info("Executing all() with delay $delayMs")
+
         return messageService.all().onEach {
+            if (delayMs >= 1000L) {
+                logger.info("issued httpbin.org/delay")
+                val response = restTemplate.exchange(
+                    "https://httpbin.org/delay/${delayMs / 1000L}",
+                    HttpMethod.GET,
+                    null,
+                    String::class.java
+                )
+                logger.info("response ${response.statusCode}")
+            }
             logger.info("Found Message $it")
         }
     }
